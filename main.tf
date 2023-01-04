0d81c0c5d2ebeb8ba6fd383f8d5a31ad77ef0189
chadm9 <chadm_@yahoo.com>
2023-01-04 17:23:28 -0500

    add remote backend

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "example" {
-  name     = "testGroup"
+resource "azurerm_resource_group" "rg1" {
+  name     = "terraform-rg1"
   location = "East US"
 }
--- providers.tf
+++ providers.tf
@@ -5,6 +5,12 @@ terraform {
       version = ">=2.0"
     }
   }
+  backend "azurerm" {
+#    resource_group_name  = "manual"
+#    storage_account_name = "tfstate142023"
+#    container_name       = "tfstateblob"
+#    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
